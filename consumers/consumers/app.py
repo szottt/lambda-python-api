510223697d90307787980cf037ea816b364c902e
Igor Szot <szottt@hotmail.com>
2024-04-22 17:21:05 -0300

    Feat: consumers and used black to python files

---
+++ consumers/consumers/app.py
@@ -0,0 +1,110 @@
+from chalice import Chalice
+
+app = Chalice(app_name="consumers")
+
+users = {
+    "users": [
+        {"name": "usuário1", "phone": "47999999999"},
+        {"name": "usuário2", "phone": "47999999999"},
+        {"name": "usuário3", "phone": "47999999999"},
+    ]
+}
+
+
+companies = {
+    "companies": [
+        {"name": "empresa1", "telefone": "47999999999"},
+        {"name": "empresa2", "telefone": "47999999999"},
+        {"name": "empresa3", "telefone": "47999999999"},
+    ]
+}
+
+
+@app.route("/consumers/person", methods=["POST"])
+def create_user():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Usuario {requests_params} criado com sucesso!",
+    }
+    return response
+
+
+@app.route("/consumers/person", methods=["PUT"])
+def put_user():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Usuario {requests_params} atualizado com sucesso!",
+    }
+    return response
+
+
+@app.route("/consumers/person", methods=["DELETE"])
+def delete_user():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Usuario {requests_params} deletado com sucesso!",
+    }
+    return response
+
+
+@app.route("/consumers/person", methods=["GET"])
+def get_user():
+    response = {"statusCode": 200, "body": users}
+    return response
+
+
+@app.route("/consumers/person/{id}", methods=["GET"])
+def get_user_id(id):
+    response = {
+        "statusCode": 200,
+        "body": {id: {"name": "usuário1", "phone": "47999999999"}},
+    }
+    return response
+
+
+@app.route("/consumers/company", methods=["POST"])
+def create_company():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Empresa {requests_params} criada com sucesso!",
+    }
+    return response
+
+
+@app.route("/consumers/company", methods=["PUT"])
+def put_company():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Empresa {requests_params} atualizada com sucesso!",
+    }
+    return response
+
+
+@app.route("/consumers/company", methods=["DELETE"])
+def delete_company():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Empresa {requests_params} deletada com sucesso!",
+    }
+    return response
+
+
+@app.route("/consumers/company", methods=["GET"])
+def get_user():
+    response = {"statusCode": 200, "body": users}
+    return response
+
+
+@app.route("/consumers/company/{id}", methods=["GET"])
+def get_user_id(id):
+    response = {
+        "statusCode": 200,
+        "body": {id: {"name": "empresa1", "telefone": "47999999999"}},
+    }
+    return response
---
+++ consumers/consumers/tests/test_app.py
@@ -0,0 +1,100 @@
+from chalice.test import Client
+from app import app
+import json
+
+
+def test_create_person():
+    with Client(app) as client:
+        response = client.http.post(
+            "/consumers/person",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_put_person():
+    with Client(app) as client:
+        response = client.http.put(
+            "/consumers/person",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_delete_person():
+    with Client(app) as client:
+        response = client.http.delete(
+            "/consumers/person",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_get_person():
+    with Client(app) as client:
+        response = client.http.get("/consumers/person")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_getid_person():
+    with Client(app) as client:
+        response = client.http.get("/consumers/person/1")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+##################################################################################### valida
+
+
+def test_create_company():
+    with Client(app) as client:
+        response = client.http.post(
+            "/consumers/company",
+            body=json.dumps({"name": "empresa1", "telefone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_put_company():
+    with Client(app) as client:
+        response = client.http.put(
+            "/consumers/company",
+            body=json.dumps({"name": "empresa1", "telefone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_delete_company():
+    with Client(app) as client:
+        response = client.http.delete(
+            "/consumers/company",
+            body=json.dumps({"name": "empresa1", "telefone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_get_company():
+    with Client(app) as client:
+        response = client.http.get("/consumers/company")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_getid_person():
+    with Client(app) as client:
+        response = client.http.get("/consumers/company/1")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
