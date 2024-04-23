89c764ef657decc3d81415aaa8b20e91a0931d41
Igor Szot <szottt@hotmail.com>
2024-04-22 23:25:11 -0300

    feat: Sales

--- sales/sales/app.py
+++ sales/sales/app.py
@@ -1,29 +1,106 @@
 from chalice import Chalice
 
+
 app = Chalice(app_name="sales")
 
 
-@app.route("/")
-def index():
-    return {"hello": "world"}
+sales = {
+    "sales": [
+        {"consumer": "usuário1", "value": "12,50", "food": "pizza"},
+        {"consumer": "usuário2", "value": "11,80", "food": "estrogonofe"},
+        {"consumer": "usuário3", "value": "10,90", "food": "macarronada"},
+    ]
+}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route("/sales/offline", methods=["POST"])
+def create_sale():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Venda {requests_params} criada com sucesso!",
+    }
+    return response
+
+
+@app.route("/sales/offline", methods=["PUT"])
+def create_sale():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Venda {requests_params} alterada com sucesso!",
+    }
+    return response
+
+
+@app.route("/sales/offline", methods=["DELETE"])
+def create_sale():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Venda {requests_params} deletada com sucesso!",
+    }
+    return response
+
+
+@app.route("/sales/offline", methods=["GET"])
+def get_sale():
+    response = {"statusCode": 200, "body": sales}
+    return response
+
+
+@app.route("/sales/offline/{id}", methods=["GET"])
+def get_sale(id):
+    response = {
+        "statusCode": 200,
+        "body": {id: {"consumer": "usuário3", "value": "10,90", "food": "macarronada"}},
+    }
+    return response
+
+
+##################################################### Online ###################################################################################
+
+
+@app.route("/sales/online", methods=["POST"])
+def create_sale():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Venda {requests_params} criada com sucesso!",
+    }
+    return response
+
+
+@app.route("/sales/online", methods=["PUT"])
+def create_sale():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Venda {requests_params} alterada com sucesso!",
+    }
+    return response
+
+
+@app.route("/sales/online", methods=["DELETE"])
+def create_sale():
+    requests_params = app.current_request.json_body
+    response = {
+        "statusCode": 200,
+        "body": f"Venda {requests_params} deletada com sucesso!",
+    }
+    return response
+
+
+@app.route("/sales/online", methods=["GET"])
+def get_sale():
+    response = {"statusCode": 200, "body": sales}
+    return response
+
+
+@app.route("/sales/online/{id}", methods=["GET"])
+def get_sale(id):
+    response = {
+        "statusCode": 200,
+        "body": {id: {"consumer": "usuário3", "value": "10,90", "food": "macarronada"}},
+    }
+    return response
--- sales/sales/tests/test_app.py
+++ sales/sales/tests/test_app.py
@@ -1,8 +1,100 @@
 from chalice.test import Client
 from app import app
+import json
 
 
-def test_index():
+def test_post_offline():
     with Client(app) as client:
-        response = client.http.get("/")
-        assert response.json_body == {"hello": "world"}
+        response = client.http.post(
+            "/sales/offline",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_put_offline():
+    with Client(app) as client:
+        response = client.http.put(
+            "/sales/offline",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_delete_offline():
+    with Client(app) as client:
+        response = client.http.delete(
+            "/sales/offline",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_get_offline():
+    with Client(app) as client:
+        response = client.http.get("/sales/offline")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_getid_offline():
+    with Client(app) as client:
+        response = client.http.get("/sales/offline/1")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+########################################## ONLINE ##########################################
+
+
+def test_post_online():
+    with Client(app) as client:
+        response = client.http.post(
+            "/sales/online",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_put_online():
+    with Client(app) as client:
+        response = client.http.put(
+            "/sales/online",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_delete_online():
+    with Client(app) as client:
+        response = client.http.delete(
+            "/sales/online",
+            body=json.dumps({"name": "usuário1", "phone": "47999999999"}),
+            headers={"Content-Type": "application/json"},
+        )
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_get_online():
+    with Client(app) as client:
+        response = client.http.get("/sales/online")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
+
+
+def test_getid_online():
+    with Client(app) as client:
+        response = client.http.get("/sales/online/1")
+        print(response.json_body)
+        assert response.json_body["statusCode"] == 200
